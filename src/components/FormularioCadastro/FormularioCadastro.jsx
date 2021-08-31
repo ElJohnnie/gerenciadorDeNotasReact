01f9d64f446bbb90c6abb5eeacc720dd7c2345b3
ElJohnnie <johnfollmer1995@gmail.com>
2021-08-31 12:37:20 -0300

    componentDidMount e ciclo de vida dos componentes

--- src/components/FormularioCadastro/FormularioCadastro.jsx
+++ src/components/FormularioCadastro/FormularioCadastro.jsx
@@ -6,6 +6,15 @@ class FormularioCadastro extends Component {
     this.titulo = "";
     this.texto = "";
     this.categoria = "Sem Categoria";
+    this.state = {categorias:[]};
+  }
+
+  componentDidMount(){
+    this.props.categorias.inscrever(this._novasCategorias.bind(this));
+  }
+
+  _novasCategorias(categorias){
+    this.setState({...this.state, categorias})
   }
 
   _handleMudancaCategoria(evento){
@@ -37,8 +46,8 @@ class FormularioCadastro extends Component {
         >
           <option>Sem Categoria</option>
 
-          {this.props.categorias.map((categoria) => {
-            return <option>{categoria}</option>;
+          {this.state.categorias.map((categoria, index) => {
+            return <option key={index}>{categoria}</option>;
           })}
         </select>
         <input
--- src/dados/Notas.js
+++ src/dados/Notas.js
@@ -7,6 +7,7 @@ export default class ArrayDeNotas{
     adicionarNota(titulo, texto, categoria){
         const novaNota = new Nota(titulo, texto, categoria);
         this.notas.push(novaNota);
+        this.notificar();
     }
 
     apagarNota(indice){
@@ -19,7 +20,7 @@ export default class ArrayDeNotas{
 
     notificar(){
         this._inscritos.forEach(func =>{
-            func(this.categorias);
+            func(this.notas);
         } );
     }
 }
